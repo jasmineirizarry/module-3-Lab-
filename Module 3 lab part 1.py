609c23fba10f007589381c80006a94c9ae0bff0b
jasmineirizarry <72569201+jasmineirizarry@users.noreply.github.com>
2020-10-20 20:32:25 -0500

    Add files via upload

---
+++ Module 3 lab part 1.py
@@ -0,0 +1,35 @@
+#Module3 Lab
+#JasmineIrizarry
+#10/20/2020
+#we have to help the player in deciding if they want to continue or not
+
+#player_actions
+
+#if they want to play again
+    #player needs to press Y for Yes
+#if the player does not want to play again
+    #player needs to press n for No
+#using terms: if or else statements
+
+
+
+yes = "Y"
+No= "n"
+
+
+user_input= input("Would you like to play game again? Type y for yes or n for NO: \n"))
+
+
+if user input == Yes:
+    print("Play game again")
+if user_input == No:
+    print("Exit game to menu")
+
+if user_inputer= yes and no:
+    print("you have to type y or n for yes or no")
+
+else:
+    print("invalid response, please choose yes or no to continue or leave game")
+
+
+#def would not work in this situation
---
+++ module 3 lab part 2 shopping.py
@@ -0,0 +1,26 @@
+#shop.py
+#Module3 Lab part 2 
+#JasmineIrizarry
+#10/20/2020
+#we have to help the person shopping be able to pay or not be able to pay
+
+
+
+def check_money(total_cost, customer_money):
+
+def check_money (total cost: $120, customer_money: $100)
+Can_pay = check_money(120, 100)
+print(cannot_pay)
+#the customer cannot pay the total price
+
+def check_money (total cost: $120, customer_money: $150)
+Can_pay = check_money(120, 150)
+print(can_pay)
+#the customer can pay the total price, will have 30$ left over.
+
+
+
+
+
+
+
